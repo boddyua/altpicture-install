01b1a9a642aed7b27726bb41dd5db86b8d03a455
Minich Bohdan <2boddy@gmail.com>
2019-11-16 11:00:01 +0200

    v.1.2.14 added update script

---
+++ update-altpicture.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root".
+    exit 1
+fi
+
+MAINDIR=/var/www/html
+if [ ! -d "$MAINDIR" ]; then
+    echo "Destination directory doesnt exist. Update stopped"
+    exit 1
+fi
+
+
+########################################## html
+UPDATEDIR=_update
+
+mkdir -p $UPDATEDIR
+cp -R ./html/* $UPDATEDIR
+find ./$UPDATEDIR -name "*.json" -delete
+
+rm -r $MAINDIR/static
+rm -r $MAINDIR/*.*
+
+cp -R ./$UPDATEDIR/* $MAINDIR
+chown -R www-data:www-data $MAINDIR/
+
+rm -r $UPDATEDIR
+
+
+echo "  Altpicture updated. " ######################################################################
