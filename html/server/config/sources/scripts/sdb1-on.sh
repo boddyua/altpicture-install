1bf4d54f397509cfb0ad5947bf72f6c718334252
Minich Bohdan <2boddy@gmail.com>
2019-11-19 18:02:19 +0200

    Fix scripts for cyrilic

--- html/server/config/sources/scripts/sdb1-on.sh
+++ html/server/config/sources/scripts/sdb1-on.sh
@@ -2,11 +2,12 @@
 
 DEVICE=sdb1
 FOLDER=/media
+OPTIONS="-o iocharset=utf8,codepage=1251"
 
 if [ -b /dev/$DEVICE ]; then
     if ! mountpoint -q -- "$FOLDER/$DEVICE" ; then
         mkdir -p $FOLDER/$DEVICE
-        mount -t auto /dev/$DEVICE $FOLDER/$DEVICE
+        mount -t auto /dev/$DEVICE $FOLDER/$DEVICE $OPTIONS
     fi
 else
     if [ -d $FOLDER/$DEVICE ]; then
--- html/server/config/sources/scripts/sdc1-on.sh
+++ html/server/config/sources/scripts/sdc1-on.sh
@@ -2,11 +2,12 @@
 
 DEVICE=sdc1
 FOLDER=/media
+OPTIONS="-o iocharset=utf8,codepage=1251"
 
 if [ -b /dev/$DEVICE ]; then
     if ! mountpoint -q -- "$FOLDER/$DEVICE" ; then
         mkdir -p $FOLDER/$DEVICE
-        mount -t auto /dev/$DEVICE $FOLDER/$DEVICE
+        mount -t auto /dev/$DEVICE $FOLDER/$DEVICE $OPTIONS
     fi
 else
     if [ -d $FOLDER/$DEVICE ]; then
@@ -14,5 +15,6 @@ else
         rm -d $FOLDER/$DEVICE
     fi
 fi
+
 
 
